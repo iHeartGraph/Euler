ca0de547bb5dd13dc58ad7322142f5e3c513fa6b
Isaiah King <iking5@gemini0.ece.seas.gwu.edu>
2023-01-24 15:19:03 -0500

    hotfix

--- lanl_experiments/euler_times.sh
+++ lanl_experiments/euler_times.sh
@@ -1,9 +1,9 @@
-python run.py -w 16 -e GCN
-python run.py -w 16 -e SAGE 
-python run.py -w 16 -e GAT 
-python run.py -w 16 -e GCN -r LSTM 
-python run.py -w 16 -e SAGE -r LSTM  
-python run.py -w 16 -e GAT -r LSTM 
-python run.py -w 16 -e GCN -r NONE 
-python run.py -w 16 -e SAGE -r NONE 
-python run.py -w 16 -e GAT -r NONE 
+python run.py -w 16 -e GCN --tests 5
+python run.py -w 16 -e SAGE --tests 5
+python run.py -w 16 -e GAT --tests 5 
+python run.py -w 16 -e GCN -r LSTM --tests 5 
+python run.py -w 16 -e SAGE -r LSTM --tests 5  
+python run.py -w 16 -e GAT -r LSTM --tests 5 
+python run.py -w 16 -e GCN -r NONE --tests 5 
+python run.py -w 16 -e SAGE -r NONE --tests 5 
+python run.py -w 16 -e GAT -r NONE --tests 5 
